fc19ee86b1dbf02dbcabd38a032644a1b64bbbf2
Maciej Mazuro <maciej.mazuro@student.wat.edu.pl>
2021-08-26 08:48:14 +0200

    wyświetlenie statystyk oraz usunięcie błędu dotyczacego braku 'populate_dict_with_module_objects'

--- projekt.py
+++ projekt.py
@@ -1,4 +1,18 @@
+#from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard
+#from keras.engine.topology import Network
+#from keras.layers import *
+#from keras.models import Model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import load_img
+#import keras.backend as K
+
+#import matplotlib.pyplot as plt
+
+import numpy as np
 import os
+import random
+import scipy.misc
+#from tqdm import *
 
 # Wskazanie ścieżki do zbiorów danych zawierający obrazy
 zbiór_danych = "./tiny-imagenet-200"
@@ -8,22 +22,22 @@ zbiór_testowy = os.path.join(zbiór_danych, "test") # zbiór danych testowych
 rozmiar_obrazów = (64, 64) # określenie rozmiaru pojedyńczego obrazka
 
 def wczytanie_zbiorów(liczba_obrazów_w_zbiorze_treningowym=10, liczba_obrazów_testowych=500):
-	X_treningowe = []
-	X_testowe = []
+	X_treningowe = [] # stworzenie macierzy obrazów treningowych
+	X_testowe = [] # stworzenie macierzy obrazów testowych
 
 	# stworzenie zbioru treningowego
 	for c in os.listdir(zbiór_treningowy):
-		c_zbiór = os.path.join(zbiór_treningowy, c, 'obrazy')
+		c_zbiór = os.path.join(zbiór_treningowy, c, 'images')
 		c_obrazy = os.listdir(c_zbiór)
 		random.shuffle(c_obrazy)
 		for nazwa_obrazu in c_obrazy[0:liczba_obrazów_w_zbiorze_treningowym]:
-			obraz = image.load.img(os.path.join(c_zbiór, nazwa_obrazu))
+			obraz = image.load_img(os.path.join(c_zbiór, nazwa_obrazu))
 			x = image.img_to_array(obraz)
 			X_treningowe.append(x)
 	random.shuffle(X_treningowe)
 
 	# stworzenie zbioru testowego
-	testowy_zbiór = os.path.join(zbiór_testowy, 'obrazy')
+	testowy_zbiór = os.path.join(zbiór_testowy, 'images')
 	obrazy_testowe = os.listdir(testowy_zbiór)
 	random.shuffle(obrazy_testowe)
 	for nazwa_obrazu in obrazy_testowe[0:liczba_obrazów_testowych]:
@@ -32,3 +46,15 @@ def wczytanie_zbiorów(liczba_obrazów_w_zbiorze_treningowym=10, liczba_obrazów
 		X_testowe.append(x)
 
 	return np.array(X_treningowe), np.array(X_testowe)
+
+# wczytanie zbioru danych
+X_treningowe_oryginał, X_testowe_oryginał = wczytanie_zbiorów()
+
+# normalizacja wektorów obrazowych
+X_treningowe = X_treningowe_oryginał/255.
+X_testowe = X_testowe_oryginał/255.
+
+# wyświetlenie statystyki
+print("Liczba przykładów treningowych = " + str(X_treningowe.shape[0]))
+print("Liczba przykładów testowych = " + str(X_treningowe.shape[0]))
+print("Wymiary zbioru treningowego: " + str(X_treningowe.shape))
